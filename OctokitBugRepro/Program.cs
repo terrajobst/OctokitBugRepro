bdd6b3fe31f64e65d581fb83f7df91d7fdf38244
Immo Landwerth <immol@microsoft.com>
2019-11-14 15:58:58 -0800

    Fix repro

--- OctokitBugRepro/Program.cs
+++ OctokitBugRepro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,7 +22,71 @@ namespace OctokitBugRepro
 
             Console.WriteLine("Get members...");
             var members = await client.Organization.Team.GetAllMembers(msTeam.Id, new TeamMembersRequest(TeamRoleFilter.Member));
-            Console.WriteLine(members.Count);
+            var logins = new SortedSet<string>(members.Select(m => m.Login));
+            Console.WriteLine(logins.Count);
+
+            var folks = logins.Where(m => m.StartsWith("opb", StringComparison.OrdinalIgnoreCase))
+                              .ToArray();
+
+            foreach (var f in folks)
+                Console.WriteLine(f);
+
+            // prints:
+            // 665
+            // // no users
+            //
+            // Here is what it should display:
+            // 1246 // that's the number of members according to the web UI
+            // opbld10
+            // opbld11
+            // opbld12
+            // opbld13
+            // opbld14
+            // opbld15
+            // opbld16
+            // opbld17
+            // opbld18
+            // opbld19
+            // opbld20
+            // opbld21
+            // opbld22
+            // opbld25
+            // opbld26
+            // opbld27
+            // opbld28
+            // opbld29
+            // opbld30
+            // opbld31
+            // opbld32
+            // opbld33
+            // opbld34
+            // opbld35
+            // opbld36
+            // opbld37
+            // opbld38
+            // opbld39
+            // opbld40
+            // opbld41
+            // opbld42
+            // opbld43
+            // opbld44
+            // opbld45
+            // opbld47
+            // opbld48
+            // opbld49
+            // opbld50
+            // opbld51
+            // opbld52
+            // opbld53
+            // opbld54
+            // opbld55
+            // opbld56
+            // opbldsb1
+            // opbldsb2
+            // opbldsb3
+            // opbldsb6
+            // opbldsb7
+            // opbldsb9
         }
     }
 }
